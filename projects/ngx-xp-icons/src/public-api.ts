aaf6653458664aa526317a4d91c2517caad946cf
Sofia Vicedomini <11537307+blacksoulgem95@users.noreply.github.com>
2024-12-25 14:51:34 +0100

    init

--- projects/ngx-xp-icons/src/public-api.ts
+++ projects/ngx-xp-icons/src/public-api.ts
@@ -4,3 +4,6 @@
 
 export * from './lib/ngx-xp-icons.service';
 export * from './lib/ngx-xp-icons.component';
+export * from './lib/xp-icon/xp-icon.type'
+export * from './lib/xp-icon/xp-icon.component'
+export * from './lib/twok-icon/twok-icon.component'
