f1bbe0e4ea742d3766977847320025f684c3e3b3
Sofia Vicedomini <11537307+blacksoulgem95@users.noreply.github.com>
2024-12-27 03:45:27 +0100

    docs init

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {IconComponent, IconPack, iconPacks, IconPackXp} from 'ngx-xp-icons'
+import {NgForOf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    NgForOf,
+    IconComponent,
+    FormsModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
 
+  iconPacks = iconPacks
+  searchValue: string = '';
+
+  get packs(): IconPack[] {
+    return Object.keys(iconPacks) as IconPack[]
+  }
+
+  getNamesForPack(pack: IconPack): IconPackXp[] {
+    return Object.keys(iconPacks[pack].files).filter(icon => this.searchValue ? icon.indexOf(this.searchValue) >= 0 : true) as IconPackXp[]
+  }
 }
