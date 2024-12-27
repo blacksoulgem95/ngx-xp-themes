32b972b97297a2552190744dcc50ba8af6b06bef
Sofia Vicedomini <11537307+blacksoulgem95@users.noreply.github.com>
2024-12-27 05:11:28 +0100

    configuration for base path

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import {routes} from './app.routes';
+import {provideClientHydration} from '@angular/platform-browser';
+import {environment} from '../environments/environment';
+import {ICON_BASE_PATH_FOR_ASSETS} from 'ngx-xp-icons';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({eventCoalescing: true}), provideRouter(routes), provideClientHydration(),
+    {provide: ICON_BASE_PATH_FOR_ASSETS, useValue: environment.basePath}]
 };
